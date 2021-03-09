4d525dc1dd69cd258d434fd0dfe2ac667efc7d89
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2021-03-09 17:44:05 -0600

    added a form to add new products and submit to the db

--- index.js
+++ index.js
@@ -23,12 +23,38 @@ app.set('views', path.join(__dirname, 'views'));
 // set view engine to ejs //
 app.set('view engine', 'ejs');
 
+app.use(express.urlencoded({ extended: true }));
+
+
 // basic routes with async/await api //
 app.get('/products', async (req, res) => {
    // query the product model //
    const products = await Product.find({}) // asks for every product //
    // this gives us access to the db product list //
    res.render('products/index', {products});
+})
+
+// route for submitted form //
+app.post('/products', async (req, res) => {
+   // db query to add new product //
+   const newProduct = new Product(req.body);
+   await newProduct.save();
+   res.redirect(`/products/${newProduct._id}`);
+
+})
+
+// route for creating new products //
+app.get('/products/new', (req, res) => {
+   // renders tml template identified //
+   res.render('products/new')
+})
+
+// route to view details about a single product //
+app.get('/products/:id', async(req, res) => {
+   const {id} = req.params;
+   // db query //
+   const product = await Product.findById(id); 
+   res.render('products/show', { product }); 
 })
 
 
